067a9623b8e89212b94bf969428a3ea318431a72
jinwls <myoungjin.choi2@gmail.com>
2024-12-13 17:15:12 +0900

    example dag

--- dags/example1.py
+++ dags/example1.py
@@ -1,14 +1,17 @@
 from datetime import datetime, timedelta
 
+from common.slack import CallbackNotifier
+
 from airflow.models.dag import DAG
+from airflow.decorators import task
 from airflow.operators.bash import BashOperator
 
 default_args = {
     "depends_on_past": False,
-    "email": ["airflow@example.com"],
-    "retries": 1,
-    "retry_delay": timedelta(minutes=5),
+    # "retries": 1,
+    # "retry_delay": timedelta(minutes=5),
     "start_date": datetime(2022, 1, 1),
+    "on_failure_callback": CallbackNotifier().on_failure_callback,
 }
 
 with DAG(
@@ -16,8 +19,15 @@ with DAG(
     default_args=default_args,
     catchup=False,
 ) as dag:
-    t1 = BashOperator(task_id="t1", bash_command="sleep 5 && echo task 1")
+    
+    @task
+    def example_failed_task():
+        from airflow.exceptions import AirflowFailException
+        raise AirflowFailException(f"testing")
+
+
+
     t2 = BashOperator(task_id="t2", bash_command="sleep 5 && echo task 2")
     t3 = BashOperator(task_id="t3", bash_command="sleep 15 && echo task 3")
     t4 = BashOperator(task_id="t4", bash_command="echo task 4")
-    t1 >> [t2, t3] >> t4
+    [t2, t3] >> t4 >> example_failed_task()
